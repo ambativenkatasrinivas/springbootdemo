2a138a7f57aeda05ba19a48a6181cb049a9a584a
ambativenkatasrinivas <ambati.srinivas45@gmail.com>
2017-07-21 11:44:12 +0530

    2 added

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -26,7 +26,7 @@ public class EmployeeController {
     public List<Employee> getEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
-		employeesList.add(new Employee(1,"lokesh","Azure git deployment1","howtodoinjava@gmail.com"));
+		employeesList.add(new Employee(1,"lokesh","Azure git deployment12","howtodoinjava@gmail.com"));
 		return employeesList;
     }
 	
