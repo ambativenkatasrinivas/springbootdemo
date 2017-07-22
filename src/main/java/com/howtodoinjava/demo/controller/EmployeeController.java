8660308b9e65e95c3c072f856dad5ef0262c1edf
ambativenkatasrinivas <ambati.srinivas45@gmail.com>
2017-07-22 11:50:04 +0530

    ; removed

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.howtodoinjava.demo.model.Employee;
+import com.howtodoinjava.demo.model.Employee
 
 @RestController
 public class EmployeeController {
